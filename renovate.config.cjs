5fdb9d3ed4768d0b0ec7581affb3e35faf2e6f25
Jonah Snider <jonah@jonahsnider.com>
2026-08-03 14:15:25 -0700

    fix: disable Renovate on forks
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- renovate.config.cjs
+++ renovate.config.cjs
@@ -2,5 +2,6 @@ module.exports = {
 	customEnvVariables: {
 		YARNSW_COREPACK_COMPAT: 'true',
 	},
+	forkProcessing: 'disabled',
 	gitIgnoredAuthors: ['29139614+renovate[bot]@users.noreply.github.com'],
 };
